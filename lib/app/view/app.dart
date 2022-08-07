79aaaa5437a1dc9e4783c9f2cb6f8dc85e192102
SamuelMauricioL <samuelmauriciolaime@gmail.com>
2022-08-07 17:44:50 -0500

    chore: add dependencies

--- lib/app/view/app.dart
+++ lib/app/view/app.dart
@@ -7,8 +7,8 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
-import 'package:flutter_web3_demo/counter/counter.dart';
 import 'package:flutter_web3_demo/l10n/l10n.dart';
+import 'package:flutter_web3_demo/screens/home_screen.dart';
 
 class App extends StatelessWidget {
   const App({super.key});
@@ -27,7 +27,7 @@ class App extends StatelessWidget {
         GlobalMaterialLocalizations.delegate,
       ],
       supportedLocales: AppLocalizations.supportedLocales,
-      home: const CounterPage(),
+      home: const Homescreen(),
     );
   }
 }
